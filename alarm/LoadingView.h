057c21807f2539f8f524aa3c0be0054bbc5cca42
wxkingstar <wx012@hotmail.com>
2013-04-15 09:48:05 +0800

    加入异步请求

---
+++ alarm/LoadingView.h
@@ -0,0 +1,13 @@
+//
+//  LoadingView.h
+//  alarm
+//
+//  Created by 王 鑫 on 13-4-14.
+//  Copyright (c) 2013年 王 鑫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoadingView : UIView
+
+@end
---
+++ alarm/LoadingView.m
@@ -0,0 +1,31 @@
+//
+//  LoadingView.m
+//  alarm
+//
+//  Created by 王 鑫 on 13-4-14.
+//  Copyright (c) 2013年 王 鑫. All rights reserved.
+//
+
+#import "LoadingView.h"
+
+@implementation LoadingView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
